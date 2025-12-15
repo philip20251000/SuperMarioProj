9e1a18ea24c5fae48096194334c11028c08f8ac1
Philip1024 <26pac01@stu.cpsk12.org>
2025-12-15 12:03:27 -0600

    bug fix

--- SuperMarioBrosTests/SuperMarioBrosTests.cpp
+++ SuperMarioBrosTests/SuperMarioBrosTests.cpp
@@ -116,95 +116,9 @@ public:
 	TEST_METHOD(BBupdate)
 	{
 
-<<<<<<< HEAD
 	}
 	TEST_METHOD(BBcollide)
 	{
-=======
-		}
-		TEST_METHOD(CoinDeathPoints)
-		{
-			Game* game = new Game();
-			int x = game->getPoints();
-			Coin test;
-			test.death();
-			int y = game->getPoints() - x;
-			Assert::AreEqual(100, y);
-			delete game;
-		}
-		TEST_METHOD(CoinDeathDecon)
-		{
-			Coin* test = new Coin();
-			Assert::IsNotNull(test);
-			int x = test->getEntities().size();
-			test->death();
-			int y = test->getEntities().size();
-			Assert::AreEqual(x - y, 1);
-		}
-		TEST_METHOD(EntityVector)
-		{
-			sf::View* viewport = new sf::View();
-			Coin* test = new Coin();
-			Goomba* test2 = new Goomba(100, false);
-			Player* test3 = new Player(viewport);
-			std::vector<Entity*> entities = test->getEntities();
-			test2->death();
-			test3->death();
-			Assert::AreEqual(1, (int)entities.size());
-			Assert::IsNotNull(dynamic_cast<Coin*> (entities.at(0)));
-			delete viewport;
-			delete test;
-		}
-		TEST_METHOD(PlayerGroundCollision)
-		{
-			sf::View* viewport = new sf::View();
-			Player* test = new Player(viewport);
-			test->setFrame(Player::FrameType::CROUCH);
-			test->update();
-			test->setFrame(Player::FrameType::STAND_LEFT);
-			test->update();
-			test->setFrame(Player::FrameType::STAND_RIGHT);
-			test->update();
-			test->setFrame(Player::FrameType::WALK_LEFT);
-			test->update();
-			test->setFrame(Player::FrameType::WALK_RIGHT);
-			Assert::IsNotNull(test->getSprite());
-			for (int i = 0; i < test->getGrounds().size(); i++)
-				Assert::IsTrue(test->getSprite()->getGlobalBounds().findIntersection(test->getGrounds().at(i)) == std::nullopt);
-		}
-		TEST_METHOD(TickUpdate)
-		{
-			sf::View* viewport = new sf::View();
-			Player* test = new Player(viewport);
-			test->setFrame(Player::FrameType::CROUCH);
-			test->update();
-			Assert::AreEqual(test->getTick(), (unsigned int)1);
-			test->setFrame(Player::FrameType::STAND_LEFT);
-			test->update();
-			Assert::AreEqual(test->getTick(), (unsigned int)2);
-		}
-		TEST_METHOD(BBSpawned)
-		{
-			BreakableBrick brick(true, true);
-			Assert::AreEqual(true, brick.getMystery());
-			Assert::AreEqual(true, brick.getCoin());
-		}
-		TEST_METHOD(BBCoin)
-		{
-			BreakableBrick brick(false, true);
-			Coin coin = brick.spawnCoin();
-			Assert::AreEqual(100, coin.getValue());
-		}
-		TEST_METHOD(BBMushroom)
-		{
-			BreakableBrick brick(true, false);
-			Goomba mushroom = brick.spawnMushroom();
-			Assert::AreEqual(mushroom.getIsMushroom(), true);
-			Assert::AreNotEqual(mushroom.getVelo().size.x, 0.f);
-		}
-		TEST_METHOD(BBupdate)
-		{
->>>>>>> 3ba893a91d62c392549812675435082126b3837c
 
 	}
 	TEST_METHOD(BBdeath)
