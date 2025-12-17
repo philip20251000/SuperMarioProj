1b38b4d2b9ec63bffad91736eaaefdea1a49555f
Philip1024 <26pac01@stu.cpsk12.org>
2025-12-17 11:03:03 -0600

    bug fix

--- SuperMarioCode/Entity/Entity.cpp
+++ SuperMarioCode/Entity/Entity.cpp
@@ -12,11 +12,6 @@ Entity::~Entity()
 }
 
 
-std::vector<Entity*> Entity::getEntities()
-{
-	return entities;
-}
-
 
 void Entity::setVelo(sf::FloatRect newVelo)
 {
@@ -24,24 +19,7 @@ void Entity::setVelo(sf::FloatRect newVelo)
 }
 
 
-sf::FloatRect Entity::getVelo()
-{
-	return velo;
-}
-
-
 void Entity::setPos(sf::FloatRect newPos)
 {
 	pos = newPos;
-}
-
-
-sf::FloatRect Entity::getPos()
-{
-	return pos;
-}
-
-const sf::Sprite* Entity::getSprite()
-{
-	return sprite;
 }
